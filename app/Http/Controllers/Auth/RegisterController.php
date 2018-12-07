ea252a517ec242974a7f24933fb1ffca07594583
juanigr <juanigreene7@gmail.com>
2018-12-07 12:30:56 -0300

    retoques al register y styles del header

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -56,8 +56,40 @@ class RegisterController extends Controller
            'password' => ['required', 'string', 'min:6', 'max:14', 'confirmed'],
            // 'country' => ['required'],
            'age' => ['required'],
-       ]);
+       ],
+     [
+       'name.required' => 'Debe ingresar un nombre',
+       'name.string' => 'El nombre no debe contener números',
+       'name.max' => 'El nombre no puede contener más de 255 caracteres',
+
+       'userName.required' => 'Debe ingresar un nombre de usuario',
+       'userName.string' => 'El nombre de usuario no puede contener números',
+       'userName.min' => 'El usuario debe contener al menos 6 caracteres',
+       'userName.unique' => 'Ya existe un usuario registrado con este nombre',
+
+       'email.required' => 'Debe ingresar un email',
+       'email.string' => 'El email no puede ser un número',
+       'email.email' => 'Debe ingresar un email válido',
+       'email.max' => 'El correo debe contener menos de 255 caracteres',
+       'email.unique' => 'Ya existe un usuario registrado con ese correo electrónico',
+
+       'avatar.required' => 'Debe ingresar una imagen',
+       'avatar.mimes' => 'El formato de la imagen no es válido. Debe ser jpeg, png, jpg o gif',
+
+       'password.required' => 'Debe ingresar una contraseña',
+       'password.string' => 'La contraseña debe contener letras',
+       'password.min' => 'La contraseña debe tener al menos 6 caracteres',
+       'password.max' => 'La contraseña no puede contener más de 14 caracteres',
+       'password.confirmed' => 'Las contraseñas no coinciden',
+
+       'age.required' => 'Debe ingresar una fecha de nacimiento',
+     ]);
+     if ($request->file('avatar')) {
+       $path = $request->file('avatar')->storeAs('avatars', $request->user()->email);
+     }
    }
+
+
 
    /**
     * Create a new user instance after a valid registration.
@@ -71,7 +103,7 @@ class RegisterController extends Controller
            'name' => $data['name'],
            'userName' => $data['userName'],
            'email' => $data['email'],
-           'avatar' => $data['avatar'],
+           'avatar' => $path??null,
            'password' => Hash::make($data['password']),
            // 'country' => $data['country'],
            'age' => $data['age'],
