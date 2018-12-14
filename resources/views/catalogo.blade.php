9d36b389982e0e9df67de44b49f0565b72186c1a
juanigr <juanigreene7@gmail.com>
2018-12-14 13:11:20 -0300

    html y css del catalogo

--- resources/views/catalogo.blade.php
+++ resources/views/catalogo.blade.php
@@ -3,39 +3,64 @@
 @section('section')
 
   <main class="mainCatalogo">
-    <div class="contenedor-productos" >
+    <div class="contenedor-principal" >
       <h2>Filtros</h2>
       <div class="filtros" >
-        <ul><br>
-          <?php foreach ($categories as $category): ?>
+        <div class="categorias">
+          <h3>CATEGORIAS</h3>
+          <ul>
+            <?php foreach ($categories as $category): ?>
             <label for=""><input type="checkbox" name="" value=""> {{ $category->name }} </label> <br><br>
-          <?php endforeach; ?>
-          <?php foreach ($ingredients as $ingredient): ?>
-            <label for=""><input type="checkbox" name="" value=""> {{ $ingredient->name }} </label> <br><br>
-          <?php endforeach; ?>
+            <?php endforeach; ?>
+          </ul>
+        </div>
 
-        </ul>
+        <div class="ingredientes">
+          <h3>INGREDIENTES</h3>
+          <ul>
+            <?php foreach ($ingredients as $ingredient): ?>
+              <label for=""><input type="checkbox" name="" value=""> {{ $ingredient->name }} </label> <br><br>
+            <?php endforeach; ?>
+          </ul>
+        </div>
       </div>
-      <h2>Catalogo de productos</h2>
+
+      <h2>CATALOGO</h2>
       <div class="catalogo" >
         @foreach ($products as $product)
           <div class="producto">
             <div class="producto-1">
-              <img width="100px" height="100px" src="{{ Storage::url($product->image) }}">
+              <img width="100px" height="100px" src="
+              @if ( $product->image == 'avatars/default.jpg' )
+                {{ 'avatars/default.jpg' }}
+              @else
+                {{ Storage::url($product->image) }}
+              @endif">
             <a href="verProducto.php"></a>
             </div>
             <div class="producto-2">
               <h4>{{$product->name}}</h4>
               <p>{{$product->description}}</p>
+              <a href="/verProducto/{{$product->id}}" class="">Ver Detalle</a>
+              <form action="" method="POST">
+                <input name="quantity" type="hidden" value="1" />
+                <input name="product_id" type="hidden" value="{{$product->id}}" />
+                <input name="product_name" type="hidden" value="{{$product->name}}" />
+                <button class="btn btn-danger" id="botonCarrito" type="submit">Agregar al Carrito</button>
+              </form>
             </div>
           </div>
 
         @endforeach
+        <div class="paginacion">
+          {{ $products->links() }}
+        </div>
 
       </div>
 
     </div>
 
   </main>
-{{ $products->links() }}
+
+
 @endsection
